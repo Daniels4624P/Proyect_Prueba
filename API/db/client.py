4635009dc8a5a21b4cfd4118f3b8a690f104ad06
Daniels4624P <96382302+Daniels4624P@users.noreply.github.com>
2024-09-06 09:45:15 -0500

    Delete API directory

--- API/db/client.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-#Base de datos local
-#db_client = MongoClient() #por defecto MongoClient se conecta a la base de datos en local
-
-URL_CONNECT = "mongodb+srv://user:Gravityfalls@proyect.hcvxo.mongodb.net/?retryWrites=true&w=majority&appName=Proyect"
-
-#Base de datos remota
-db_client = MongoClient(URL_CONNECT).test
--- API/db/models/user.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class User(BaseModel):
-    id: str | None = None # En mongo db el id es str
-    username: str
-    email: str
